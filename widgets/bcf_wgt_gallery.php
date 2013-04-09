e1e2074c3c39c345eecc504c254bb58c38bdad9b
G100g <gigetto@gmail.com>
2013-04-09 18:40:47 +0200

    Updates to Gallery widget

--- widgets/bcf_wgt_gallery.php
+++ widgets/bcf_wgt_gallery.php
@@ -18,6 +18,25 @@ class bcf_gallery extends bcf_text {
 		$value = ($value !== false ? htmlentities($value, ENT_QUOTES, get_option('blog_charset')) : '');
 		
 ?>		
+		<span id="<?php echo $name; ?>_preview">
+<?php
+		
+			if ($value != "") {
+				
+				$image_ids = explode("," ,$value);
+				foreach($image_ids as $image_id) :
+				
+				$image = wp_get_attachment_image($image_id, 'thumbnail');
+				
+?>
+				<?php echo $image; ?>	
+<?php				
+				endforeach;
+			
+			}
+?>
+		</span>
+		
 		<input type="text" id="<?php echo $id; ?>" name="<?php echo $name; ?>" value="<?php echo $value; ?>" class="full <?php echo $id. " " .$class; ?>" <?php echo ( !$active ? ' disabled="disabled"' : ''  ); ?> />
 		
 		<button id="bcf_gallery_new" class="button">Select Images</button>
@@ -27,12 +46,16 @@ class bcf_gallery extends bcf_text {
 			(function ($) {
 				$(document).ready(function(){
 				
+					$preview = $('#<?php echo $name; ?>_preview');
+				
+				
 				wp.media.bcf_gallery = {
 	     
 				    frame: function() {
 				        if ( this._frame )
 				            return this._frame;
-				 
+						
+						var selection = this.select();
 				        this._frame = wp.media({
 				            id:         'my-frame',                
 				            frame:      'post',
@@ -40,15 +63,28 @@ class bcf_gallery extends bcf_text {
 				            title:      wp.media.view.l10n.editGalleryTitle,
 				            editing:    true,
 				            multiple:   true,
+				            selection:  selection
 				        });
 				        
-				        this._frame.on( 'update', function (onon) {
+				        this._frame.on( 'update', function (attachments) {
 				        
-				        console.log(onon);
+				        	//console.log(onon);
+				        	//Preview
+				        	$preview.html("");
+				        	attachments.each(function(attachment) {
+										    
+								var url = attachment.get('sizes').thumbnail.url;    
+						        $preview.append('<img src="'+url+'" />');
+        
+						    });
+						    
+						    var ids = attachments.pluck('id');						    
+						    $('#bcf_gallery').val(ids);
 				        	
+				        	/*
 				        	var controller = wp.media.bcf_gallery._frame.states.get('gallery-edit');
 				        	
-						    console.log(controller);
+						    console.log(controller, 'controller');
 				        	
 						    var library = controller.get('library');
 						    
@@ -64,7 +100,7 @@ class bcf_gallery extends bcf_text {
 						    
 						   
 						    
-				        	gallery.each(function(attachment) {
+				        	onon.each(function(attachment) {
 						        console.log(attachment);
         // this will return an object with all the attachment-details
 						    });
@@ -78,6 +114,7 @@ class bcf_gallery extends bcf_text {
 						    var ids = library.pluck('id');
 						    
 						    $('#bcf_gallery').val(ids);
+						    */
 						    
 				        });
 				        
@@ -94,7 +131,65 @@ class bcf_gallery extends bcf_text {
 				 
 				        });
 				        
-				    }
+				    },
+				    
+				    select: function() {
+						    //[gallery ids="10,7"]
+						    var shortcode = '[gallery ids="7, 10"]',
+						        defaultPostId = wp.media.gallery.defaults.id,
+						        attachments, selection;
+						 
+						    attachments = wp.media.gallery.attachments( shortcode );
+						    selection = new wp.media.model.Selection( attachments.models, {
+						        props:    attachments.props.toJSON(),
+						        multiple: true
+						    });
+						     
+						    selection.gallery = attachments.gallery;
+						 
+						    // Fetch the query's attachments, and then break ties from the
+						    // query to allow for sorting.
+						    selection.more().done( function() {
+						        // Break ties with the query.
+						        selection.props.set({ query: false });
+						        selection.unmirror();
+						        selection.props.unset('orderby');
+						    });
+						    
+						    /*
+						    var shortcode = wp.shortcode.next( 'gallery', wp.media.view.settings.shibaMlib.shortcode ),
+						        defaultPostId = wp.media.gallery.defaults.id,
+						        attachments, selection;
+						 
+						    // Bail if we didn't match the shortcode or all of the content.
+						    if ( ! shortcode )
+						        return;
+						 
+						    // Ignore the rest of the match object.
+						    shortcode = shortcode.shortcode;
+						 
+						    if ( _.isUndefined( shortcode.get('id') ) && ! _.isUndefined( defaultPostId ) )
+						        shortcode.set( 'id', defaultPostId );
+						 
+						    attachments = wp.media.gallery.attachments( shortcode );
+						    selection = new wp.media.model.Selection( attachments.models, {
+						        props:    attachments.props.toJSON(),
+						        multiple: true
+						    });
+						     
+						    selection.gallery = attachments.gallery;
+						 
+						    // Fetch the query's attachments, and then break ties from the
+						    // query to allow for sorting.
+						    selection.more().done( function() {
+						        // Break ties with the query.
+						        selection.props.set({ query: false });
+						        selection.unmirror();
+						        selection.props.unset('orderby');
+						    });
+						 	*/
+						    return selection;
+						},
 				};
 				 
 				
